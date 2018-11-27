e3ba47c192e1c80c6b645c500283aa8d538751d6
Unknown <jautz@uos.de>
2018-11-27 12:55:06 +0100

    Changed the label of relation

--- explorer.py
+++ explorer.py
@@ -12,11 +12,12 @@ def main():
     relation_mentions = get_mentions_type(article_uri, "nwr:RelationMention")
     events = get_events(article_uri)
     events_with_agent_patient = get_events_with_agent_patient(article_uri)
-    triplets=get_triplets(article_uri)
+    triplets = get_triplets(article_uri)
     print(relation_mentions)
     print(events)
     print(events_with_agent_patient)
     print(triplets)
+
 
 def get_events(article_uri):
     """ generates list of event URIs for a given article """
@@ -47,12 +48,16 @@ def get_triplets(article_uri):
     """Generates list of triplets of (agent,event,patient) for a given article"""
     timecodes = ["#tmx" + str(i) for i in range(7)]
     queries = [
-        "SELECT DISTINCT ?event ?relation ?agent ?patient  WHERE {?event rdf:type sem:Event . ?event sem:hasAtTime <" + str(article_uri) + str(
+        "SELECT DISTINCT ?event ?relation ?agent ?patient  WHERE {?event rdf:type sem:Event . ?event sem:hasAtTime <" + str(
+            article_uri) + str(
             timecode) +
-        "> . ?event propbank:A0 ?agent . ?event propbank:A1 ?patient . ?event rdfs:label ?relation}" for timecode in timecodes]
+        "> . ?event propbank:A0 ?agent . ?event propbank:A1 ?patient . ?event rdfs:label ?relation}" for timecode in
+        timecodes]
 
-    result_list=[[result["agent"],result["event"],result["patient"]] for query in queries for result in ks.run_sparql_query(query)]
+    result_list = [[result["agent"], result["event"], result["patient"]] for query in queries for result in
+                   ks.run_sparql_query(query)]
     return result_list
+
 
 if __name__ == "__main__":
     main()
