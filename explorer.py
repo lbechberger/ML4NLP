fa9daeccb7323a0e667aa5db5f8e86e41fb94242
Unknown <jautz@uos.de>
2018-11-27 15:13:13 +0100

    Formatting Hook

--- explorer.py
+++ explorer.py
@@ -4,6 +4,7 @@ from knowledgestore import ks
 
 all_article_uris = pd.read_csv("all_article_uris.csv")
 
+
 def get_events(article_uri):
     """ generates list of event URIs for a given article """
     timecodes = ["#tmx" + str(i) for i in range(7)]
@@ -30,22 +31,24 @@ def get_mentions_type(article_uri, type):
 
 
 def get_triple_from_event(event_uri):
-	query = "SELECT DISTINCT ?agent ?charloc ?patient  WHERE {<" + event_uri + "> propbank:A0 ?agent . <" + event_uri + "> propbank:A1 ?patient . <" + event_uri + "> gaf:denotedBy ?charloc}"
-	result = ks.run_sparql_query(query)
-	if len(result) == 0:
-		return ()
-	else:
-		agent = (result[0]["agent"].split("/")[-2], result[0]["agent"].split("/")[-1].replace("+", " "))
-		patient = (result[0]["patient"].split("/")[-2], result[0]["patient"].split("/")[-1].replace("+", " "))
-		charlocs = [(int(r["charloc"].split("=")[-1].split(",")[0]), int(r["charloc"].split("=")[-1].split(",")[1])) for r in result if r["charloc"].split("#")[0] == event_uri.split("#")[0]]
-		return (agent, charlocs, patient)
+    query = "SELECT DISTINCT ?agent ?charloc ?patient  WHERE {<" + event_uri + "> propbank:A0 ?agent . <" + event_uri +\
+            "> propbank:A1 ?patient . <" + event_uri + "> gaf:denotedBy ?charloc}"
+    result = ks.run_sparql_query(query)
+    if len(result) == 0:
+        return ()
+    else:
+        agent = (result[0]["agent"].split("/")[-2], result[0]["agent"].split("/")[-1].replace("+", " "))
+        patient = (result[0]["patient"].split("/")[-2], result[0]["patient"].split("/")[-1].replace("+", " "))
+        charlocs = [(int(r["charloc"].split("=")[-1].split(",")[0]), int(r["charloc"].split("=")[-1].split(",")[1])) for
+                    r in result if r["charloc"].split("#")[0] == event_uri.split("#")[0]]
+        return (agent, charlocs, patient)
 
 
 def generate_triples_from_uri(uri):
-	events = get_events(uri)
-	triples = []
-	for event in events:
-		trip = get_triple_from_event(event)
-		if len(trip) > 0:
-			triples.append(trip)
-	return triples
+    events = get_events(uri)
+    triples = []
+    for event in events:
+        trip = get_triple_from_event(event)
+        if len(trip) > 0:
+            triples.append(trip)
+    return triples
