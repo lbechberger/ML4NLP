01e767dc6e7cbd6716d12ec36121aae7a24bcc5b
Nathanaelion <martin.poemsl@gmx.de>
2018-11-30 11:08:55 +0100

    removed redundand files and redundand loop in data generator

--- data_generator.py
+++ data_generator.py
@@ -12,10 +12,13 @@ all_article_uris = explorer.all_article_uris
 
 
 def main():
-	data = generate_data_for_articles([all_article_uris.loc[i]["article"] for i in range(3)])
+	data = generate_data_for_articles([all_article_uris.loc[i]["article"] for i in range(2)])
 	data.to_csv("test_data.csv")
 	print(data.head())
 	print(data.describe())
+	print("----------"")
+	print(data["classification"].value_counts())
+	# generate_data_chunks(star_index, end_index)
 
 
 def generate_data_for_articles(articles):
@@ -23,25 +26,24 @@ def generate_data_for_articles(articles):
 	for article_uri in articles:
 		triples = explorer.generate_triples_from_uri(article_uri)
 		article_text = ks.run_files_query(article_uri)
+		splits_at = get_positions_of_char_in_text(" ", article_text)
+		words_by_position = [(splits_at[x] + 1, splits_at[x + 1]) for x in range(len(splits_at) - 1)]
 		for triple in triples:
-			tdata = generate_classification_data(triple[0], triple[2], triple[1], article_text)
+			tdata = generate_classification_data(triple[0], triple[2], triple[1], article_uri, words_by_position)
 			data = data.append(tdata,ignore_index=True)
 	return data
 
 
-def generate_all_data():
+def generate_data_chunks(start_index, end_index):
 	pass
 
 
-def generate_classification_data(agent, patient, chars, raw_text):
-	correct_relations = [(c[0], c[1]) for c in chars]
-	splits_at = get_positions_of_char_in_text(" ", raw_text)
-	words_by_position = [(splits_at[x] + 1, splits_at[x + 1]) for x in range(len(splits_at) - 1)]
+def generate_classification_data(agent, patient, correct_relations, article_uri, words_by_position):
 	result_frame = pd.DataFrame()
 	for word_by_position in words_by_position:
 		classification = word_by_position in correct_relations
-		pandas_line = pd.DataFrame({"agent": agent[1], "patient": patient[1], "word_start_char": word_by_position[0], "word_end_char": word_by_position[1], "classification": classification, "raw_text": raw_text}, index=[0])
-		result_frame = result_frame.append(pandas_line, ignore_index=True)
+		word_row = pd.DataFrame({"agent": agent[1], "patient": patient[1], "word_start_char": word_by_position[0], "word_end_char": word_by_position[1], "classification": classification, "article_uri": article_uri}, index=[0])
+		result_frame = result_frame.append(word_row, ignore_index=True)
 	return result_frame
 
 
