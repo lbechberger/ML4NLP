1c72bbad4b51e0f25b02ae58d593161a6aa71fa1
Chris Stenkamp <cstenkamp@uos.de>
2018-11-30 19:12:44 +0100

    getting entity information

--- weekfour.py
+++ weekfour.py
@@ -19,11 +19,12 @@ def main():
     uri = "http://en.wikinews.org/wiki/SEALs_say_US_officer's_cover-up_was_reported_by_fake_SEAL"
     mentions = ks.run_resource_query(uri, 'ks:hasMention')
     article = article_cache[uri]
-    print("EVENTS:")
-    for mention in mentions:
-        entity = get_event(article, mention)
-        if entity:
-            print(entity)
+    # print("EVENTS:")
+    # for mention in mentions:
+    #     entity = get_event(article, mention)
+    #     if entity:
+    #         print(entity)
+    entities = [get_entity(article, mention) for mention in mentions if get_entity(article, mention)]
     print("ENTITIES:")
     for mention in mentions:
         entity = get_entity(article, mention)
@@ -58,8 +59,51 @@ def get_entity(article, mention):
     if tmp and "http://dbpedia.org/resource/" in tmp:
         word, sent = get_word_and_sentence(article, mention)
         stem = ks.run_mention_query(mention, 'nwr:pred')
+        additional_info =  get_entity_info(tmp)
         if sent:
-            return "Entity:", stem or word, tmp.replace("http://dbpedia.org/resource/", ""), sent
+            return "Entity:", stem or word, tmp.replace("http://dbpedia.org/resource/", ""), sent, additional_info
+
+
+def run_dictless_query(query, prefix_dict = None):
+    variable_names = query.split("SELECT")[1].split("WHERE")[0].strip()
+    assert len(variable_names.split(","))
+    if prefix_dict:
+        prefix_str = "".join(["PREFIX "+key+": <"+value+"> \n" for key, value in list(prefix_dict.items())])
+        query = prefix_str + query
+    res = ks.run_sparql_query(query)
+    return [i[variable_names[1:]] for i in res]
+
+
+def get_entity_info(entity):
+    #TODO den scheiß cachen, und dinge wie birthname undso nur dann überhaupt erst querien wenn's nen Mensch ist
+    entity_info = {}
+    entity = entity.replace('http://dbpedia.org/resource/', 'dbpedia:')
+    prefix_dict = {'dbp': 'http://dbpedia.org/ontology/', 'dc': 'http://purl.org/dc/elements/1.1/', 'gaf': 'http://groundedannotationframework.org/gaf#'}
+
+    entity_info['types'] = run_dictless_query("SELECT ?l WHERE {"+entity+" rdf:type ?l}") \
+        + run_dictless_query('SELECT ?l WHERE {'+entity+' dc:description ?l }', prefix_dict)
+
+    entity_info['altnames'] = run_dictless_query('SELECT ?l WHERE {'+entity+' rdfs:label ?l}') \
+        + run_dictless_query('SELECT ?l WHERE {'+entity+' dbp:alias ?l }', prefix_dict) \
+        + run_dictless_query('SELECT ?l WHERE {'+entity+' dbp:alternativeName ?l }', prefix_dict) \
+        + run_dictless_query('SELECT ?l WHERE {'+entity+' dbp:birthName ?l }', prefix_dict)
+
+    entity_info['birtdate'] = run_dictless_query('SELECT ?l WHERE {'+entity+' dbp:birthDate ?l }', prefix_dict) \
+        + run_dictless_query('SELECT ?l WHERE {'+entity+' dbp:birthYear ?l }', prefix_dict)
+
+    entity_info['birtplace'] = run_dictless_query('SELECT ?l WHERE {' + entity + ' dbp:birthPlace ?l }', prefix_dict)
+
+    entity_info['country'] = run_dictless_query('SELECT ?l WHERE {' + entity + ' dbp:country ?l }', prefix_dict) \
+        + run_dictless_query('SELECT ?l WHERE {' + entity + ' dbp:nationality ?l }', prefix_dict) \
+
+    entity_info['gender'] = run_dictless_query('SELECT ?l WHERE {' + entity + ' dbp:gender ?l }', prefix_dict)
+
+    entity_info['profession'] = run_dictless_query('SELECT ?l WHERE {' + entity + ' dbp:profession ?l }', prefix_dict)
+
+    entity_info['mentioned_in'] = run_dictless_query('SELECT ?l WHERE {'+entity+' gaf:denotedBy ?l }', prefix_dict)
+
+    return entity_info
+
 
 
 def get_e(mention):
