069f45bd4bb490bd4b73cc34cc6632ee19b8ac2c
Christopher Gundler <c.gundler@mail.de>
2018-12-05 22:41:55 +0100

    Add improved dataset

--- create_data.py
+++ create_data.py
@@ -4,17 +4,29 @@ from modules.data import DataSet
 from src.create_dataset import load_categories
 from src import PATH_CONFIG, PATH_RAW_DATA, PATH_DATASETS
 
+SEED = 42
+MIN_ARTICLES = 5
+
 if __name__ == "__main__":
     assert PATH_CONFIG.exists(), "Invalid config path!"
 
+    # Create raw data cache, if needed
     if not PATH_RAW_DATA.exists():
         PATH_RAW_DATA.mkdir()
 
+    # Create dataset dir, if needed
     if not PATH_DATASETS.exists():
         PATH_DATASETS.mkdir()
 
-    interests = load_categories(PATH_RAW_DATA / "articles.pickle", 5)
+    # Load all available categories from cache or web and filter for those with at least MIN_ARTICLES
+    interests = load_categories(PATH_RAW_DATA / "articles.pickle", MIN_ARTICLES)
 
-    dataset = DataSet.Parameter.from_json(PATH_CONFIG / "dataset.json").generate(interests)
-    dataset_path = dataset.save(PATH_DATASETS)
-    dataset.as_dataframe().to_csv(dataset_path / "dataset.csv", sep="\t")
+    # Load the dataset parameter from the config file and generate the data with a specific seed
+    data = DataSet.Parameter.from_json(PATH_CONFIG / "dataset.json").generate(
+        interests, SEED
+    )
+
+    # Save the data and export it into human-readable *.csv
+    data_path = data.save(PATH_DATASETS)
+    for dataset_name, dataset in data.items():
+        dataset.to_csv(data_path / "{}.csv".format(dataset_name), sep="\t", index=False)
--- modules/data/dataset.py
+++ modules/data/dataset.py
@@ -1,54 +1,108 @@
 import random
 import pickle
 import json
+import random
+from collections import UserDict
 
 import pandas as pd
+from sklearn.utils import shuffle
+import numpy as np
 
 from . import User, Representation
 from pathlib import Path
 
-class DataSet:
+
+class DataSet(UserDict):
     class Parameter:
-        def __init__(self, user_params, num_user):
+        def __init__(self, user_params, num_user, splits):
             self.user_params = user_params
             self.num_user = num_user
-            
-        def generate(self, interests):
-            return DataSet(
-                [self.user_params.generate(interests) for _ in range(self.num_user)],
-                self
+            self.splits = splits
+
+        def generate(self, interests, seed=None):
+            # Set seed, if specified
+            if seed is not None:
+                np.random.seed(seed)
+                random.seed(seed)
+
+            # Generate all the users
+            users = [self.user_params.generate(interests) for _ in range(self.num_user)]
+
+            # Create the names for the columns
+            column_names = (
+                ["interest_{}".format(i) for i in range(self.user_params.num_interests)]
+                + [
+                    "article_{}".format(i)
+                    for i in range(
+                        self.user_params.representation_params.num_articles_per_interest
+                        * self.user_params.num_interests
+                    )
+                ]
+                + ["candidate", "label"]
             )
+
+            # Generate the actual dataset
+            data = pd.DataFrame.from_records(
+                (
+                    [interest for interest in interests]
+                    + [article for article in articles]
+                    + [candidate]
+                    + [label]
+                    for user in users
+                    for interests, articles, candidate, label in user.rows()
+                ),
+                columns=column_names,
+            )
+
+            # Shuffle the data
+            data = shuffle(data)
+
+            # Generate the splits
+            index = 0
+            result = {}
+            for key, value in self.splits.items():
+                size = int(len(data) * value)
+                result[key] = data.iloc[
+                    index : int(index + len(data) * value)
+                ].reset_index(drop=True)
+                index += size
+
+            return DataSet(dataframes=result, hyperparameters=self)
 
         @staticmethod
         def from_json(path):
-            with open(path, 'r') as json_file:
+            with open(path, "r") as json_file:
                 data = json.load(json_file)
 
-                user_params = data['user']
-                representation_params = user_params['representation']
+                user_params = data["user"]
+                representation_params = user_params["representation"]
 
                 return DataSet.Parameter(
                     user_params=User.Parameter(
                         representation_params=Representation.Parameter(
-                            num_articles_per_interest=representation_params['articles_per_interest'], 
-                            num_positive_samples=representation_params['positive_samples'], 
-                            num_negative_samples=representation_params['negative_samples']
-                        ), 
-                        num_interests=user_params['interest'], 
-                        num_representations=user_params['representations']
+                            num_articles_per_interest=representation_params[
+                                "articles_per_interest"
+                            ],
+                            num_positive_samples=representation_params[
+                                "positive_samples"
+                            ],
+                            num_negative_samples=representation_params[
+                                "negative_samples"
+                            ],
+                        ),
+                        num_interests=user_params["interest"],
+                        num_representations=user_params["representations"],
                     ),
-                    num_user=data['users']
+                    num_user=data["users"],
+                    splits=data["splits"],
                 )
 
-    DATASET_NAME =  "Dataset_{}"
+    DATASET_NAME = "Dataset_{}"
 
-    def __init__(self, users, hyperparameters):
-        self.users = users
+    def __init__(self, dataframes, hyperparameters):
+        super().__init__(dataframes)
         self.params = hyperparameters
-        
-    def __str__(self):
-        return "Dataset with {} users.".format(len(self.users))
-    
+
     def save(self, path, filename="dataset.pickle"):
         num_existing_datasets = len([True for f in path.iterdir() if f.is_dir()])
 
@@ -61,34 +115,17 @@ class DataSet:
 
     @staticmethod
     def get_last_dataset(path):
-        candidates = {int(f.name.split('_')[1]): f for f in path.iterdir() if f.is_dir()}
+        candidates = {
+            int(f.name.split("_")[1]): f for f in path.iterdir() if f.is_dir()
+        }
         return candidates[max(candidates.keys())]
 
     @staticmethod
-    def load(path, version = None, filename="dataset.pickle"):
-        dataset_path = DataSet.get_last_dataset(path) if version is None else path / DataSet.DATASET_NAME.format(version)
+    def load(path, version=None, filename="dataset.pickle"):
+        dataset_path = (
+            DataSet.get_last_dataset(path)
+            if version is None
+            else path / DataSet.DATASET_NAME.format(version)
+        )
         with open(dataset_path / filename, "rb") as file:
             return pickle.load(file)
-        
-    def as_dataframe(self):
-        column_names = [
-            "interest_{}".format(i)
-            for i in range( 
-                self.params.user_params.num_interests
-            )
-        ] + [
-            "article_{}".format(i)
-            for i in range(
-                self.params.user_params.representation_params.num_articles_per_interest * 
-                self.params.user_params.num_interests
-            )
-        ] + ["candidate", "label"]
-        
-        return pd.DataFrame.from_records((
-            [str(interest) for interest in interests] + 
-            [article.url for article in articles] + 
-            [candidate.url] + 
-            [label]
-            for user in self.users 
-            for interests, articles, candidate, label in user.rows()
-        ), columns=column_names)
