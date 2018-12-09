58cefa9b988a89f386c8cc02a4c25c051ac54f34
Jannik Schmitt <jannik.schmitt@t-online.de>
2018-12-09 19:32:39 +0100

    revamped question_generation to work with group delta's dataset

--- data_generation/question_generation.py
+++ data_generation/question_generation.py
@@ -1,57 +1,78 @@
 import knowledgestore.ks as ks
-from lxml import etree as ET
+import csv
+import sys
+import random
 
 
-def generate_natural_questions(triple):
-    """Generates a set of possible human-like questions for elements in a given RDF-style triple."""
-    # @TODO: implement this function and alter write_into_xml so that it is compatible
-    pass
+fieldnames = ["event_id", "article_id", "uri", "event", "agent", "predicate", "patient"]
+event_ids = []
+article_ids = []
+uris = []
+events = []
+agents = []
+predicates = []
+patients = []
 
 
-def generate_artificial_questions(triple):
-    """Generates a set of possible query-like questions for elements in a given RDF-style triple."""
-    questions = []
-    triple_temp = triple.split()
-    questions.append("(?, " + triple_temp[1] + ", " + triple_temp[2] + ")")
-    questions.append("(" + triple_temp[0] + ", ?, " + triple_temp[2] + ")")
-    questions.append("(" + triple_temp[0] + ", " + triple_temp[1] + ", ?)")
-    return questions
+def naively_generate_negatives(event_pos):
+    """Generates negative examples by taking correct triples, checking other entities from a list of all entities and substituting one of the entities in the triple with another non-matching one from the list."""
+    actual_agent = agents[event_pos]
+    actual_patient = patients[event_pos]
+    mode = random.randint(1,2)
+    # @TODO: evaluate how random this actually is or if it generates a bias towards either subjects or objects
+    same_as_before = True
+    if mode == 1:
+        while same_as_before:
+            print(actual_agent)
+            new_agent_id = random.randint(0, len(agents) - 1)
+            replacement_agent = agents[new_agent_id]
+            print(replacement_agent)
+            if replacement_agent != actual_agent: 
+                same_as_before = False
+        # returns a dict that can be written into the csv file
+        return {"event_id": event_ids[event_pos], "article_id": article_ids[event_pos], "uri": uris[event_pos], "event": events[event_pos], "agent": replacement_agent, "predicate": predicates[event_pos], "patient": patients[event_pos]}
+    else:
+        while same_as_before:
+            new_patient_id = random.randint(0, len(patients) - 1)
+            print(actual_patient)
+            replacement_patient = patients[new_patient_id]
+            print(replacement_patient)
+            if replacement_patient != actual_patient:
+                same_as_before = False
 
-    
-def write_into_xml(q_a_pairs, triples, filename):
-    """Used to write the information that we generated into an XML file."""
-    dataset = ET.Element("dataset")
-    levels = []
-    for index in range(len(triples)):
-        levels.append(ET.SubElement(dataset, "qa_pair_" + str(index)))
-        ET.SubElement(levels[index], "triple").text = triples(index)
-        ET.SubElement(levels[index], "subject_question").text = q_a_pairs[index][0]
-        ET.SubElement(levels[index], "relation_question").text = q_a_pairs[index][1]
-        ET.SubElement(levels[index], "object_question").text = q_a_pairs[index][2]
-    tree = ET.ElementTree(dataset)
-    tree.write(filename + ".xml", pretty_print=True)
+        return {"event_id": event_ids[event_pos], "article_id": article_ids[event_pos], "uri": uris[event_pos], "event": events[event_pos], "agent": agents[event_pos], "predicate": predicates[event_pos], "patient": replacement_patient}      
 
 
-def generate_negatives(triples):
+def smartly_generate_negatives(event_pos):
     """Generates negative examples by taking correct triples, checking other entities from the source text of a triple
     and substituting one of the entities in the triple with another non-matching one from the text."""
     pass
 
 
-def test():
-    """Used to test the functionality of this file."""
-    questions = ["Barack_Obama father_of Malia_Obama", "Michelle_Obama mother_of Malia_Obama"]
-    for item in questions:
-        print("Generating questions...")
-        n_q_a_pairs.append(generate_artificial_questions(item))
-    write_into_xml(n_q_a_pairs, questions, "test")
-
-
 if __name__ == '__main__':
-    n_q_a_pairs = [] # natural question-answer pairs
-    a_q_a_pairs = [] # artificial question-answer pairs
-    with open('triples.txt', 'r') as f:
-        for triple in f:
-            n_q_a_pairs.append(generate_natural_questions(triple))
-            a_q_a_pairs.append(generate_artificial_questions(triple))
-    f.close()
+    # sets integer determining amount of negatives generated to a value determined by the user
+    # if user omits a value, it is automatically set to 10
+    if len(sys.argv) != 1:
+        generation_factor = sys.argv[1]
+    else:
+        generation_factor = 10
+    
+    # read in the information from the csv file
+    with open("demo_triples.csv") as csvDataFile:
+        data_file = csv.reader(csvDataFile)
+        for row in data_file:
+            event_ids.append(row[0])
+            article_ids.append(row[0])
+            uris.append(row[0])
+            events.append(row[0])
+            agents.append(row[0])
+            predicates.append(row[0])
+            patients.append(row[0])
+    
+    # start writing a new csv file
+    with open("negatives.csv", "w") as csvDataFile:
+        writer = csv.DictWriter(csvDataFile, fieldnames=fieldnames)
+        writer.writeheader()
+        for event_pos in range(len(events)):
+            for n in range(int(generation_factor)):
+                writer.writerow(naively_generate_negatives(event_pos))
