89cf7f25e19a58568658ab845c50400f57e8f5be
AnnaP <anpki@users.noreply.github.com>
2019-01-10 09:00:19 +0100

    Added pickle files with embedded articles
    Had to split them up because if size issues

--- user_generation.py
+++ user_generation.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import gensim
 from generation_functions import *
 from sklearn.model_selection import KFold
 
@@ -10,57 +9,67 @@ np.random.seed(0)
 # load categories, articles and URIs from file
 categories, uris, _ = pickle.load(open("user_articles.pickle", "rb"))
 # get texts of all articles. Will be dumped in pickle file to avoid unnecessary computations when re-running code
-# articles = get_articles_from_link(uris)
-articles = pickle.load(open("articles.pickle", "rb"))
+if os.path.isfile('./articles.pickle'):
+    articles = pickle.load(open("articles.pickle", "rb"))
+else:
+    articles = get_articles_from_link(uris)
+
 
 # create a dataframe with 100 users (rows) who get randomly assigned 0 or 1 for each category (cols)
 # correct subcategory labels if superior category is disliked (=0)
-# users_db = pd.DataFrame(np.random.randint(2, size=(100, len(categories))), columns=categories)
-# users_db.apply(lambda row: check_subcategories(row), axis=1)
+users_db = pd.DataFrame(np.random.randint(2, size=(100, len(categories))), columns=categories)
+users_db.apply(lambda row: check_subcategories(row), axis=1)
 
-# print("\nThere are {} categories. In total {} articles resulting in a dataset length of {}"
-#       .format(len(categories), len(articles), len(input_data)))
 
-# get the word2vec of each category name for later comparison in feature extraction process
-# if os.path.isfile('./embed_categories.pickle'):
-#     print("Found embed_categories.pickle file. Continuing ..")
-#     embedded_categories = pickle.load(open("embed_categories.pickle", "rb"))
-# else:
-#     print("Did not find embed_categories.pickle file. Create embedding of categories ..")
-#     embedded_categories = []
-#     embedding = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True).wv
-#     for c in categories:
-#         embedded_categories.append(get_w2v_string(c, embedding))
-#     pickle.dump(embedded_categories, open("embed_categories.pickle", "wb"))
+# Embedding of each category
+embedding = []
+if os.path.isfile('./embed_categories.pickle'):
+    print("Found embed_categories.pickle file. Continuing ..")
+    embedded_categories = pickle.load(open("embed_categories.pickle", "rb"))
+else:
+    print("Did not find embed_categories.pickle file. Create embedding of categories ..")
+    embedded_categories = []
+    embedding = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True).wv
+    for c in categories:
+        embedded_categories.append(get_w2v_string(c, embedding))
+    pickle.dump(embedded_categories, open("embed_categories.pickle", "wb"))
 
-embedding = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True).wv
-a_counter = 0
-embedded_articles = []
+
+# Embedding of each article
 if os.path.isfile('./embed_articles.pickle'):
     embedded_articles, a_counter = pickle.load(open("embed_articles.pickle", "rb"))
+else:
+    a_counter = 0
+    embedded_articles = []
+    if not embedding:
+        embedding = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin',
+                                                                    binary=True).wv
+    for a in articles[a_counter:]:
+        embed = get_w2v_string(a, embedding)
+        mean_embed = [sum(x)/len(x) for x in zip(*embed)]
+        embedded_articles.append(mean_embed)
+        print("Article: {} /{}".format(a_counter, len(articles)))
+        pickle.dump([embedded_articles, a_counter], open("embed_articles.pickle", "wb"))
+        a_counter += 1
 
-for a in articles[a_counter:]:
-    embed = get_w2v_string(a, embedding)
-    mean_embed = [sum(x)/len(x) for x in zip(*embed)]
-    embedded_articles.append(mean_embed)
-    print("Article: {} /{}".format(a_counter, len(articles)))
-    a_counter += 1
-    pickle.dump([embedded_articles, a_counter], open("embed_articles.pickle", "wb"))
+print("Number of embedded articles: {} with following dimensions: {}".format(len(embedded_articles),
+                                                                             len(embedded_articles[0])))
 
 
 # split dataset into test and training data via k-fold
-# input_data, labels = create_dataset(users_db, articles, 100)
-# kf = KFold(n_splits=10, shuffle=True)
-# for train_index, test_index in kf.split(input_data):
-#     X_train = []
-#     X_test = []
-#     y_train = []
-#     y_test = []
-#     for i in train_index:
-#         X_train.append(input_data[i])
-#         y_train.append(labels[i])
-#     for j in test_index:
-#         X_test.append(input_data[j])
-#         y_test.append(labels[j])
-#     print(len(X_train))
+input_data, labels = create_dataset(users_db, articles, 100)
+kf = KFold(n_splits=10, shuffle=True)
+for train_index, test_index in kf.split(input_data):
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+    for i in train_index:
+        X_train.append(input_data[i])
+        y_train.append(labels[i])
+    for j in test_index:
+        X_test.append(input_data[j])
+        y_test.append(labels[j])
 
+print("\nThere are {} categories. In total {} articles resulting in a dataset length of {}"
+      .format(len(categories), len(articles), len(input_data)))
